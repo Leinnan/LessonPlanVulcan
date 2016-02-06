881d87c31adf31f79421a5f77de89f80e4ab772d
Leinnan <rybopiotr@gmail.com>
2016-02-06 12:47:18 +0100

    czyszczenie kodu

--- main.py
+++ main.py
@@ -6,33 +6,33 @@ import lesson
 import argparse
 
 parser = argparse.ArgumentParser(description='Pokazuje plan lekcji',
-                                    epilog='Program stworzony przez Leinnana.')
-parser.add_argument('-c', '--current', dest='current_only', help='Pokazuje tylko aktualny/najblizszy dzien', action='store_true')
-parser.add_argument('-nl', '--nologo', dest='logo', help='Nie pokazuje loga', action='store_true')
+                                   epilog='Program stworzony przez Leinnana.')
+parser.add_argument('-c', '--current', dest='current_only',
+                     help='Pokazuje tylko aktualny/najblizszy dzien',
+                     action='store_true')
+parser.add_argument('-nl', '--nologo', dest='logo', 
+                    help='Nie pokazuje loga', action='store_true')
 args = parser.parse_args()
 
 
-show_current_only = parser.parse_args().current_only; 
-hide_logo = parser.parse_args().logo; 
+show_current_only = parser.parse_args().current_only
+hide_logo = parser.parse_args().logo
 
 
 
 
 logo = """\033[1m
-
     _   ___                     ___  ______ 
     | | / / |                   /   | |  ___|
     | |/ /| | __ _ ___  __ _   / /| | | |_   
     |    \| |/ _` / __|/ _` | / /_| | |  _|  
     | |\  \ | (_| \__ \ (_| | \___  | | |    
-    \_| \_/_|\__,_|___/\__,_|     |_/ \_|    
-                                         
-                                         
+    \_| \_/_|\__,_|___/\__,_|     |_/ \_|                                    
 \033[0m
 """
 
 if hide_logo == False:
     print(logo)
 
-my_lessons_array = lesson.getLessons();
-lesson.printLessons(my_lessons_array, show_current_only);
+my_lessons_array = lesson.getLessons()
+lesson.printLessons(my_lessons_array, show_current_only)
--- my_time.py
+++ my_time.py
@@ -6,7 +6,8 @@ import datetime
 
 def current_time():
     # aktualny dzien, godzina i minuta
-    current_time_array = [datetime.datetime.now().day, datetime.datetime.now().hour, datetime.datetime.now().minute];
-    return current_time_array;
-    # print(str(current_time_array[0]) + "\t" + str(current_time_array[1]) + "\t" + str(current_time_array[2]))
+    current_time_array = [datetime.datetime.now().day, 
+                            datetime.datetime.now().hour, 
+                            datetime.datetime.now().minute]
+    return current_time_array
     
